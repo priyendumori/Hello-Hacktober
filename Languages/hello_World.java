8faab0dbf8894fb4dba0f7e1a295a755f2b21f47
Priyendu Mori <priyendu.mori@gmail.com>
2018-10-18 21:14:39 +0530

    hello_World.java

---
+++ Languages/hello_World.java
@@ -0,0 +1,10 @@
+public class hello_World {
+
+    public static void main(String[] args) {
+        print("Hello World..!");        
+    }
+
+    static print(String s){
+        System.out.println(s);
+    }
+}
